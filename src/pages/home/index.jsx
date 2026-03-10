c576bbd5b793916e456bf2b11fa2f3a46a3e460d
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-10 11:44:54 -0300

    estilizacao finalizada

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,20 @@
 
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Aula de React com Vite JS</h1>
+
+      <button
+        onClick={() => navigate('/exemplo/1')}
+      >
+        Exemplo 1
+      </button>
+
     </div>
   )
 }
