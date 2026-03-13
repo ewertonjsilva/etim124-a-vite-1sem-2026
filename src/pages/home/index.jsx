583e7d7f7f44f7f437d974080bb2f5350ee82a00
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-13 14:40:43 -0300

    alinhando

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,30 @@
 
 import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router";
+
+import './index.css';
 
 function Home() {
 
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Aula de React com Vite JS</h1>
+    <div className='containerHome'>
+      <div className='containerAlinhamento'>
+        <h1>Aula de React com Vite JS</h1>
+        <div className='containerCards'>
+          <button
+            onClick={() => navigate('/exemplo/1')}
+          >
+            Exemplo 1
+          </button>
 
-      <button
-        onClick={() => navigate('/exemplo/1')}
-      >
-        Exemplo 1
-      </button>
+          <Link to="/exemplo/2">Exemplo 2</Link>
+        </div>
+
+
+
+      </div>
 
     </div>
   )
