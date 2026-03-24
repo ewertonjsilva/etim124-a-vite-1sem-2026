86ac0c5693bd2a95f631dd00aff736c3b5272e84
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-24 11:39:21 -0300

    aula 24-03

--- src/pages/atividades/atv-01/index.jsx
+++ src/pages/atividades/atv-01/index.jsx
@@ -2,7 +2,7 @@ import styles from './index.module.css';
 
 function Atividade01() {
     return (
-        <div>
+        <div className={styles.container}>
             <h1 className={styles.tituloAtv}>Atividade 1</h1>
             <p>Componente básico para renderizar tela com React</p>
         </div>
--- src/pages/exemplos/ex-03/index.jsx
+++ src/pages/exemplos/ex-03/index.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 
 import styles from './index.module.css';
 
 export default function Exemplo03() {
 
+    const [num, setNum] = useState(10);
+
+    function somarUm () {
+        setNum(num + 1);
+    }
+
     return (
         <div className={styles.container}>
-            <label className={styles.texto}>Contador</label>
+            <label className={styles.texto}>{`Contador ${num}`}</label>
             <label                
-                className={styles.botao}
+                className={styles.botao} 
+                onClick={() => somarUm()}
             >+1</label>
         </div>
     );
